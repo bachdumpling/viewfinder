e5a830da513bd3dfd64f1501d9cef0082c41f07d
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2023-11-06 11:30:45 -0500

    fix deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,21 @@ import Search from "./components/Search";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  async function getArtDetails() {
+    const artPieceName = "The+Assumption+of+the+Virgin";
+    const artist = "El+Greco";
+    const year = "1577";
+
+    const apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=${artPieceName}&q=${artist}&q=${year}&fields=id,title,artist_display,date_display,image_id,artist_title,artist_id`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    console.log(encodeURIComponent(data.data[0].title.split(' ').join('+')));
+  }
+
+  getArtDetails();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
