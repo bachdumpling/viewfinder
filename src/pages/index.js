d831f6c79ebe0b66b72ea3e1e89a808d38215c11
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2023-11-15 13:47:16 -0500

    add font

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 font-inconsolata`}
     >
       <Search />
     </main>
