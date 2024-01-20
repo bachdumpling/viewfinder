dacd306aec069c69d2cd0ed41fe6a88b74a26cdc
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2024-01-20 18:55:17 -0500

    Add Google Analytics tracking code to _app.js and index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -175,19 +175,21 @@ export default function Home() {
 
   return (
     <main className="relative min-h-screen min-w-[375px] w-screen overflow-x-hidden">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-4V7VHKCMNQ"
+      {/* <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'G-4V7VHKCMNQ');
-        `}
-      </Script>
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script> */}
       <video
         autoPlay
         loop
